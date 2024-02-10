dd474c06aeda45db2ae7ac61156c0f8324956282
ogrenci <80684257+kaplanebru@users.noreply.github.com>
2024-02-10 22:15:58 +0300

    pushing objects

--- Assets/Test/Hold.cs
+++ Assets/Test/Hold.cs
@@ -9,10 +9,12 @@ public class Hold : MonoBehaviour
     private XRSimpleInteractable grabInteractable;
     private float initialScale;
     private float maxScale = 3.0f; 
-    private float growthRate = 0.5f; 
+    public float growthRate = 0.01f;
+    public Camera cam;
 
     private void Start()
     {
+        cam = Camera.main;
         grabInteractable = GetComponent<XRSimpleInteractable>();
         initialScale = transform.localScale.x;
     }
@@ -21,7 +23,8 @@ public class Hold : MonoBehaviour
     {
         if (grabInteractable.isSelected)
         {
-            ScaleObject();
+            //ScaleObject();
+            PushObject();
         }
     }
 
@@ -32,5 +35,11 @@ public class Hold : MonoBehaviour
             float newScale = transform.localScale.x + growthRate * Time.deltaTime;
             transform.localScale = new Vector3(newScale, newScale, newScale);
         }
+    }
+
+    private void PushObject()
+    {
+        Vector3 direction = (cam.transform.position - transform.position).normalized;
+        transform.position += -direction * growthRate;
     }
 }
