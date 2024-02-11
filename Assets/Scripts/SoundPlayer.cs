4b8c9bcacd13486eedc487a788319a5eeb9bfa37
ysyucel <ysyucel@icloud.com>
2024-02-11 14:29:32 +0300

    s

---
+++ Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    // Singleton instance
+    public static SoundPlayer instance;
+
+    // The audio clip to be played
+    public AudioClip soundClip;
+
+    // Ensure only one instance of SoundPlayer exists
+    private void Awake()
+    {
+        // If an instance already exists and it's not this instance, destroy this instance
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // Set this instance as the singleton instance
+        instance = this;
+
+        // Ensure this GameObject persists across scenes
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Play the sound
+    public void PlaySound()
+    {
+        // Check if there's an audio clip assigned
+        if (soundClip != null)
+        {
+            // Create an AudioSource component if there isn't one attached
+            AudioSource audioSource = GetComponent<AudioSource>();
+            if (audioSource == null)
+            {
+                audioSource = gameObject.AddComponent<AudioSource>();
+            }
+
+            // Play the sound clip
+            audioSource.PlayOneShot(soundClip);
+        }
+        else
+        {
+            Debug.LogWarning("No audio clip assigned to play.");
+        }
+    }
+
+    // Get the singleton instance of SoundPlayer
+    public static SoundPlayer GetInstance()
+    {
+        return instance;
+    }
+}
--- Assets/Scripts/StartingSequence.cs
+++ Assets/Scripts/StartingSequence.cs
@@ -2,41 +2,94 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
+using TMPro;
 public class StartingSequence : MonoBehaviour
 {
     public GameObject shakeObject;
+    public GameObject rumbleObject;
     public UIController uiController;
     public GameObject startingMessage;
     public List<GameObject> dustParticle;
+    public GameObject coverText;
+    public GameObject findyourfamilyText;
     bool isQuake = false;
+    public TMP_Text textMeshPro;
+    public float earthquakeTime;
+    public GameObject clicktoproceedButton;
+    public AudioClip soundClipEarth;
     // Start is called before the first frame update
     void Start()
     {
-        LeanTween.delayedCall(2f,() => {
+        LeanTween.delayedCall(8f,() => {
             CloseStartingMessage();
-            StartEarthQuake();
         });
     }
     public void StartEarthQuake()
     {
         if (isQuake) { return; }
+
+        CloseStartingMessage();
+        coverText.SetActive(true);
         isQuake = true;
-        Debug.Log("EarthQuake");
-        shakeObject.GetComponent<CameraShake>().Shake();
-        shakeObject.GetComponent<ShakeSample>().StartEarthQuake();
-        foreach(GameObject item in dustParticle) { item.SetActive(true); }
-        LeanTween.delayedCall(8f, () => {
-            uiController.FadeIn();
+        LeanTween.delayedCall(3f, () => {
+            SoundPlayer.instance.soundClip = soundClipEarth;
+            SoundPlayer.instance.PlaySound();
+            Debug.Log("EarthQuake");
+            shakeObject.GetComponent<CameraShake>().shakeDuration= earthquakeTime;
+            shakeObject.GetComponent<ShakeSample>().shakeDuration=earthquakeTime;
+            shakeObject.GetComponent<CameraShake>().Shake();
+            rumbleObject.GetComponent<Rumble>().enabled = true;
+            shakeObject.GetComponent<ShakeSample>().StartEarthQuake();
+            foreach(GameObject item in dustParticle) { item.SetActive(true); }
+            LeanTween.delayedCall(earthquakeTime+1f, () => {
+                coverText.SetActive(false);
+                rumbleObject.GetComponent<Rumble>().enabled = false;
+                uiController.FadeIn();
+                ChangeStartingMessageToOk();
+                LeanTween.delayedCall(7f, () => {
+                    CloseStartingMessage();
+                    OpenFamilyText();
+                });
+            });
         });
     }
     public void CloseStartingMessage()
     {
         startingMessage.SetActive(false);
     }
-    
+    public void ChangeStartingMessageToOk()
+    {
+        textMeshPro.text = "It's over for now, check for injuries!";
+        startingMessage.SetActive(true);
+    }
+    public void OpenFamilyText()
+    {
+        
+        findyourfamilyText.SetActive(true);
+       // LeanTween.delayedCall(8f, () => {
+           // CloseFamilyText();
+        //});
+    }
+    public void CloseFamilyText()
+    {
+        findyourfamilyText.SetActive(false);
+    }
     // Update is called once per frame
     void Update()
     {
+        
+    }
+    public void Finishapp()
+    {
+        LeanTween.delayedCall(3f, () => {
+            clicktoproceedButton.SetActive(true);
+        });
+    }
+    public void LoadIsekai()
+    {
+        
+            SceneManager.LoadScene("gameScene");
         
     }
 }
